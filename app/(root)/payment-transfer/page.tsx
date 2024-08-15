e8c1c2d19c21b4e02fa3226d201dac33d3475a26
Michael Chan <michael.w.chan01@gmail.com>
2024-08-15 11:07:51 -0400

    Made header for payment transfer

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,10 +1,19 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Transfer = () => {
   return (
-    <div>
-      Transfer
-    </div>
+    <section className="payment-transfer">
+      <HeaderBox
+      title= "Payment Transfer"
+      subtext="Provide details regarding the payment transfer."
+      />
+
+      <section className="size-full pt-5">
+        
+      </section>
+
+    </section>
   )
 }
 
